16ead7f93038a677c2907ce7513bc7f783aafed0
Scottl4784 <Scottl4784@gmail.com>
2018-05-21 14:52:09 -0400

    added functionality for hit button

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ const cards = ['aceOfSpades', 'aceOfClubs', 'aceOfHearts', 'aceOfDiamonds',
 const cardsValue = [11, 11, 11, 11, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
 
 // variable to select a random card value from the arrays
-let randomCard = cardsValue[Math.floor(Math.random() * cardsValue.length)]
 
 // array holding the players current cards
 const playerCards = []
@@ -28,14 +27,19 @@ const dealerCards = []
 // When the player presses the deal button deal 2 cards to the dealer and 2 cards to the player from the cards array
 $('.deal').on('click', function () {
     for (let i = 0; i < 2; i++) {
-            playerCards.push(randomCard)
+            playerCards.push(cardsValue[Math.floor(Math.random() * cardsValue.length)])
+            dealerCards.push(cardsValue[Math.floor(Math.random() * cardsValue.length)])
     }
 })
 
+// if the player clicks the hit button deal another random card to the player and update the total
+$('.hit').on('click', function(){
+    playerCards.push(cardsValue[Math.floor(Math.random() * cardsValue.length)])
+})
 
 // assign matching values from the cardsValue array to the cards dealt to each player
 // assign the total of the player cards to the player-total class
-// if the player clicks the hit button deal another random card to the player and update the total
+
 // if the player total exceeds 21 then trigger "Bust!" toast
 // if the player clicks the stand button end player turn and go to dealer
 // assign random cards from the cards array to dealer until dealer total is greater than or equal to player total or dealer total is greater than 21
