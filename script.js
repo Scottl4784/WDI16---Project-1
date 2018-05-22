2cedff64a298ebdfe2d80c7809c9a7dec3f4306b
Scottl4784 <Scottl4784@gmail.com>
2018-05-22 09:27:40 -0400

    began working on stand function

--- script.js
+++ script.js
@@ -1,72 +1,68 @@
-// Stores each of the cards in the deck
-const cards = ['aceOfSpades', 'aceOfClubs', 'aceOfHearts', 'aceOfDiamonds',
-    'twoOfSpades', 'twoOfClubs', 'twoOfHearts', 'twoOfDiamonds',
-    'threeOfSpades', 'threeOfClubs', 'threeOfHearts', 'threeOfDiamonds',
-    'fourOfSpades', 'fourOfClubs', 'fourOfHearts', 'fourOfDiamonds',
-    'fiveOfSpades', 'fiveOfClubs', 'fiveOfHearts', 'fiveOfDiamonds',
-    'sixOfSpades', 'sixOfClubs', 'sixOfHearts', 'sixOfDiamonds',
-    'sevenOfSpades', 'sevenOfClubs', 'sevenOfHearts', 'sevenOfDiamonds',
-    'eightOfSpades', 'eightOfClubs', 'eightOfHearts', 'eightOfDiamonds',
-    'nineOfSpades', 'nineOfClubs', 'nineOfHearts', 'nineOfDiamonds',
-    'tenOfSpades', 'tenOfClubs', 'tenOfHearts', 'tenOfDiamonds',
-    'jackOfSpades', 'jackOfClubs', 'jackOfHearts', 'jackOfDiamonds',
-    'queenOfSpades', 'queenOfClubs', 'queenOfHearts', 'queenOfDiamonds',
-    'kingOfSpades', 'kingOfClubs', 'kingOfHearts', 'kingOfDiamonds']
+function deal() {
+    // Stores each of the cards in the deck
+    const cards = ['aceOfSpades', 'aceOfClubs', 'aceOfHearts', 'aceOfDiamonds',
+        'twoOfSpades', 'twoOfClubs', 'twoOfHearts', 'twoOfDiamonds',
+        'threeOfSpades', 'threeOfClubs', 'threeOfHearts', 'threeOfDiamonds',
+        'fourOfSpades', 'fourOfClubs', 'fourOfHearts', 'fourOfDiamonds',
+        'fiveOfSpades', 'fiveOfClubs', 'fiveOfHearts', 'fiveOfDiamonds',
+        'sixOfSpades', 'sixOfClubs', 'sixOfHearts', 'sixOfDiamonds',
+        'sevenOfSpades', 'sevenOfClubs', 'sevenOfHearts', 'sevenOfDiamonds',
+        'eightOfSpades', 'eightOfClubs', 'eightOfHearts', 'eightOfDiamonds',
+        'nineOfSpades', 'nineOfClubs', 'nineOfHearts', 'nineOfDiamonds',
+        'tenOfSpades', 'tenOfClubs', 'tenOfHearts', 'tenOfDiamonds',
+        'jackOfSpades', 'jackOfClubs', 'jackOfHearts', 'jackOfDiamonds',
+        'queenOfSpades', 'queenOfClubs', 'queenOfHearts', 'queenOfDiamonds',
+        'kingOfSpades', 'kingOfClubs', 'kingOfHearts', 'kingOfDiamonds']
 
-// Assigns value to each of the cards in the cards array
-const cardsValue = [11, 11, 11, 11, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
+    // Assigns value to each of the cards in the cards array
+    const cardsValue = [11, 11, 11, 11, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
 
-// array holding the players current cards
-const playerCards = []
+    // array holding the players current cards
+    const playerCards = []
 
-// array holding the dealers current cards
-const dealerCards = []
+    // array holding the dealers current cards
+    const dealerCards = []
 
-const playerTotal = playerCards.reduce((pv, cv) => {
-    return pv + (parseFloat(cv) || 0);
-}, 0);
-
-
-// When the player presses the deal button deal 2 cards to the dealer and 2 cards to the player from the cards array
-$('.deal').on('click', function () {
-    // variable to select a random card value from the arrays
-    for (let i = 0; i < 2; i++) {
-        playerCards.push(cardsValue[Math.floor(Math.random() * cardsValue.length)])
-        dealerCards.push(cardsValue[Math.floor(Math.random() * cardsValue.length)])
-    }
-})
-
-// if the player clicks the hit button deal another random card to the player and update the total
-$('.hit').on('click', function () {
-    // variable to select a random card value from the arrays
-    playerCards.push(cardsValue[Math.floor(Math.random() * cardsValue.length)])
-
-})
+    let playerTotal = playerCards.reduce((pv, cv) => {
+        return pv + (parseFloat(cv) || 0)
+    }, 0)
 
 
 
 
-// const player = {
-//     playerCards: [],
-//     playerHits: function() {
-//     }
+    // When the player presses the deal button deal 2 cards to the dealer and 2 cards to the player from the cards array
+    $('.deal').on('click', function () {
+        // variable to select a random card value from the arrays
+        for (let i = 0; i < 2; i++) {
+            if (playerCards.length < 2) {
+                playerCards.push(cardsValue[Math.floor(Math.random() * cardsValue.length)])
+                dealerCards.push(cardsValue[Math.floor(Math.random() * cardsValue.length)])
+            }
+        }
+    })
+
+    // if the player clicks the hit button deal another random card to the player and update the total
+    $('.hit').on('click', function () {
+        // if player has clicked deal and has 2 cards already allow this
+        if (playerCards.length >= 2) {
+            playerCards.push(cardsValue[Math.floor(Math.random() * cardsValue.length)])
+        }
+    })
+
+    $('.stand').on('click', function () {
+        for (let i = 0; i < dealerCards.length; i++) {
+            let dealerTotal = dealerCards.reduce((pv, cv) => {
+                return pv + (parseFloat(cv) || 0)
+            }, 0)
+            if (dealerTotal < 21) {
+                dealerCards.push(cardsValue[Math.floor(Math.random() * cardsValue.length)])
+            }
+        }
+    })
+}
 
 
-// }
-// window.onload = function () {
-//     ('.hit').on('click', function () {
-//         // variable to select a random card value from the arrays
-//         player.playerCards.push(cardsValue[Math.floor(Math.random() * cardsValue.length)])
 
-//     })
-//     $('.deal').on('click', function () {
-//         // variable to select a random card value from the arrays
-//         for (let i = 0; i < 2; i++) {
-//             playerCards.push(cardsValue[Math.floor(Math.random() * cardsValue.length)])
-//             dealerCards.push(cardsValue[Math.floor(Math.random() * cardsValue.length)])
-//         }
-//     })
-// }
 
 
 
